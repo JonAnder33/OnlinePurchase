c4451839b002c0b6cbc9467d2e9558db011c52b4
JonAnder33 <jonander.delapuebla@opendeusto.es>
2022-06-07 20:39:25 +0200

    Añadida la clase producto

--- src/main/java/com/onlinepurchase/server/data/classes/Producto.java
+++ src/main/java/com/onlinepurchase/server/data/classes/Producto.java
@@ -1,5 +1,45 @@
 package com.onlinepurchase.server.data.classes;
 
 public class Producto {
-
+	private String nombre;
+	private float precio;
+	private float peso;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public Producto(String nombre, float precio, float peso) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+	}
+	
+	public Producto() {
+		super();
+		this.nombre = "Producto";
+		this.precio = 0;
+		this.peso = 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", precio=" + precio + ", peso=" + peso + "]";
+	}
+	
+	
 }
