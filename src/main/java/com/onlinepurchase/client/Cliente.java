e492d035a805fd78896afd5fdca4abf167f608ef
JonAnder33 <jonander.delapuebla@opendeusto.es>
2022-06-13 23:20:35 +0200

    Anadido en el pom elementos y cambiada parte de cliente

--- src/main/java/com/onlinepurchase/client/Cliente.java
+++ src/main/java/com/onlinepurchase/client/Cliente.java
@@ -1,5 +1,20 @@
 package com.onlinepurchase.client;
 
+import java.util.List;
+
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
 public class Cliente {
+    Client client;
+    WebTarget webTarget;
+
+
 
 }
