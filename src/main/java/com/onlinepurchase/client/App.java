2eb5029bcb5e25d3dcb67562d641922aa8075f2c
JonAnder33 <jonander.delapuebla@opendeusto.es>
2022-06-15 16:16:39 +0200

    Creado el app

--- src/main/java/com/onlinepurchase/client/App.java
+++ src/main/java/com/onlinepurchase/client/App.java
@@ -1,13 +1,54 @@
 package com.onlinepurchase.client;
 
+
+import java.io.IOException;
+import java.net.URL;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends Application {
+    public static Stage primaryStage;
+    public static Cliente cliente;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		cliente = new Cliente("localhost", "8085");
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout(getClass().getResource("MainWindow.fxml"));
+        }
+
+	    /**
+	     * Initializes the root layout.
+	     * @param fxml El archivo cargado ya como URL
+	     */
+	    public static void initRootLayout(URL fxml) {
+	        try {
+	            // Load root layout from fxml file.
+	            FXMLLoader loader = new FXMLLoader();
+	            Scene scene = new Scene((Parent) loader.load(fxml));
+
+	            // Show the scene containing the root layout.
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+
+	        } catch (IOException e) {
+	            System.out.println("Error al iniciar la ventana");
+	        }
+	    }
 }
