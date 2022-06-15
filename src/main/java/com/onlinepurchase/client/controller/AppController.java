6446fb3e47b2947243b346ccbce5ed0c229924c2
JonAnder33 <jonander.delapuebla@opendeusto.es>
2022-06-15 16:18:01 +0200

    Creado el app controller

--- src/main/java/com/onlinepurchase/client/controller/AppController.java
+++ src/main/java/com/onlinepurchase/client/controller/AppController.java
@@ -1,5 +1,18 @@
 package com.onlinepurchase.client.controller;
 
-public class AppController {
 
+import com.onlinepurchase.client.App;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AppController {
+    @FXML
+    private Label onlinePurchaseLabel;
+
+    @FXML
+    protected void onOnlinePurchaseButtonClick() {App.initRootLayout(getClass().getResource("ProductosWindow.fxml")); }
+    
+ 
 }
