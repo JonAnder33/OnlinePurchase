608185222d5aaf118ef872ad78f9a98fb12f7cfd
JonAnder33 <jonander.delapuebla@opendeusto.es>
2022-06-15 16:28:11 +0200

    Creado el prepare data

--- src/main/java/com/onlinepurchase/util/PrepareData.java
+++ src/main/java/com/onlinepurchase/util/PrepareData.java
@@ -1,5 +1,32 @@
 package com.onlinepurchase.util;
 
-public class PrepareData {
 
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+
+import com.onlinepurchase.server.data.classes.Producto;
+
+public class PrepareData {
+	public static void main(String[] args) {
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Producto p1 = new Producto("Gominolas",2,1);
+			
+			pm.makePersistent(p1);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
 }
